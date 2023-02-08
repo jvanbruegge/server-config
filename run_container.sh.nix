e974abab3ff5ad58cb11c4add4a515bbb66efd3c
Jan van Brügge <supermanitu@gmail.com>
2023-02-08 20:12:48 +0000

    Convert shell script to nix expression

--- run_container.sh.nix
+++ run_container.sh.nix
@@ -1,4 +1,11 @@
-#!/usr/bin/env bash
+{ inPath, app, stdenv, lib }:
+let
+  path = lib.strings.concatMapStringsSep ":" (n: "${n}/bin") inPath;
+in
+''
+#!${stdenv.shell}
+
+set -o xtrace
 
 set -euo pipefail
 
@@ -8,7 +15,7 @@ cd "$dir"
 mkdir nix
 
 # shellcheck disable=2016
-unshare --pid --mount --fork --user --net --uts --cgroup --map-auto --map-root-user bash -c '
+unshare --pid --mount --fork --user --net --uts --cgroup --map-auto --map-root-user ${stdenv.shell} -c '
 # See https://github.com/NixOS/nixpkgs/issues/42117#issuecomment-974194691
 PATH=$(echo "$PATH" | sed -e "s/\/run\/wrappers\/bin://g")
 
@@ -17,14 +24,15 @@ mount --rbind /nix ./nix
 mkdir old_root proc
 pivot_root $(pwd) $(pwd)/old_root
 
-PATH=/nix/store/q6my4sv2vnddq5iyh12xfjvrnjrlq8cs-coreutils-full-9.1/bin:/nix/store/6wnx8cj5v6lzc8ih9v0dkr0cakwbf6dl-util-linux-2.38.1-bin/bin
+PATH=${path}
 
 mount -t proc proc /proc
 umount -l /old_root
 rm -d old_root
 
-exec /nix/store/x40p83k9mk03kklpfahqppp6kz85yx99-bash-interactive-5.1-p16/bin/bash
+exec ${app}
 '
 
 cd ..
 rm -r "$dir"
+''
