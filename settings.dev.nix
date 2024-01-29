9008566c5b6b00951dd8f0d8270229a3c40d64d9
Jan van Brügge <supermanitu@gmail.com>
2024-01-29 12:15:23 +0000

    tunnel: Use wireguard VPN instead of SSH reverse tunnel
    The VPN allow me to also port forward UDP ports

--- settings.dev.nix
+++ settings.dev.nix
@@ -12,28 +12,9 @@
     serveo_known_hosts = {};
   };
 
-  systemd.services.tunnel = {
-    description = "SSH reverse tunnel for ports 80 and 443";
-    after = [ "network.target" "haproxy.service" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = ''
-        ${pkgs.openssh}/bin/ssh root@${domain} \
-          -i %d/id_rsa \
-          -o ServerAliveInterval=60 \
-          -o UserKnownHostsFile=%d/known_hosts \
-          -R :80:localhost:80 -R :443:localhost:443 \
-          -R :389:localhost:389 -R :636:localhost:636 -N
-      '';
-      Restart = "always";
-      RestartSec = 5;
-      LoadCredential = [
-        "id_rsa:/run/secrets/serveo_id_rsa"
-        "known_hosts:/run/secrets/serveo_known_hosts"
-      ];
-      DynamicUser = true;
-      ProtectSystem = "strict";
-    };
+  networking.wg-quick.interfaces.wg0.configFile = "/run/secrets/wireguard";
+  sops.secrets.wireguard = {
+    format = "binary";
+    sopsFile = ./tunnel/vps.conf;
   };
 }
