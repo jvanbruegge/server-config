6c8213e385a06cad1fdad3d1108b0aeff554b067
Jan van Brügge <supermanitu@gmail.com>
2024-02-17 18:21:35 +0000

    networking: Use systemd-resolved

--- nodes/vps/configuration.nix
+++ nodes/vps/configuration.nix
@@ -28,6 +28,8 @@
 
   users = import ../../users.nix;
 
+  services.resolved.enable = true;
+  networking.nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
 
   nix.settings."trusted-users" = [ "root" "@wheel" ];
 
