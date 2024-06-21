98e63de35d0798e3b4b928afe76e62ef6862a5f1
Jan van Brügge <supermanitu@gmail.com>
2024-06-21 12:50:43 +0100

    general: Update channel for nixos-infect script

--- updatekeys.sh
+++ updatekeys.sh
@@ -8,9 +8,9 @@ if [ -n "$AGE_KEY" ]; then
   age_key=$($SSH_COMMAND 'nix-shell -p ssh-to-age --run "cat /etc/ssh/ssh_host_ed25519_key.pub | ssh-to-age"')
   sed -i "s/&${AGE_KEY}.*$/\&${AGE_KEY} ${age_key}/" .sops.yaml
   for f in ./secrets/*; do
-    sops updatekeys -y "$f"
+    sops updatekeys -y "$f" || true
   done
   for f in ./tunnel/*; do
-    sops updatekeys -y "$f"
+    sops updatekeys -y "$f" || true
   done
 fi
