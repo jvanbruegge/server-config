d37ac7c68a9e0d290904d9243c4e799ea34957ae
Jan van Brügge <supermanitu@gmail.com>
2024-06-21 12:48:45 +0100

    nix: Update nixpkgs and home-manager
    Also update list of disabled scanner devices because of https://github.com/NixOS/nixpkgs/pull/293022

--- caladan/paperless.nix
+++ caladan/paperless.nix
@@ -97,20 +97,21 @@ in {
     enable = true;
     extraBackends = [ pkgs.epkowa ];
     disabledDefaultBackends = [
-      "net" "abaton" "agfafocus" "apple" "artec"
-      "as6e" "avision" "bh" "canon"
-      "cardscan" "coolscan"
-      "dell1600n_" "dmc" "epjitsu" "epson2" "epsonds"
-      "escl" "fujitsu" "genesys" "gt68xx" "hp"
-      "hs2p" "ibm" "kodak"
+      "net" "abaton" "agfafocus" "apple" "artec" "artec_eplus48u"
+      "as6e" "avision" "bh" "canon" "canon630u" "canon_dr"
+      "canon_lide70" "cardscan" "coolscan" "coolscan3"
+      "dell1600n_net" "dmc" "epjitsu" "epson2" "epsonds"
+      "escl" "fujitsu" "genesys" "gt68xx" "hp" "hp3500"
+      "hp3900" "hp4200" "hp5400" "hp5590" "hpljm1005" "hpsj5s"
+      "hs2p" "ibm" "kodak" "kodakaio"
       "kvs1025" "kvs20xx" "kvs40xx" "leo"
       "lexmark" "ma1509" "magicolor" "matsushita" "microtek"
-      "mustek" "nec"
-      "niash" "pie" "pint" "pixma" "plustek"
-      "qcam" "ricoh" "rts8891" "s9036"
+      "microtek2" "mustek" "mustek_usb" "mustek_usb2" "nec"
+      "niash" "pie" "pieusb" "pint" "pixma" "plustek"
+      "qcam" "ricoh" "ricoh2" "rts8891" "s9036"
       "sceptre" "sharp" "sm3600" "sm3840" "snapscan"
       "sp15c" "tamarack" "teco1" "teco2" "teco3"
-      "u12" "umax" "v4l" "xerox_mfp"
+      "u12" "umax" "umax1220u" "v4l" "xerox_mfp"
     ];
   };
 
