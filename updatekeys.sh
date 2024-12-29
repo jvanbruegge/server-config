15acc74ae5a8762aadfe0320643de7fe0c7063dc
Jan van Brügge <supermanitu@gmail.com>
2024-12-29 11:43:44 +0000

    Fix regex in update keys script

--- updatekeys.sh
+++ updatekeys.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
-export SSH_COMMAND="ssh -p $SERVER_PORT root@$SERVER_ADDRESS"
+export SSH_COMMAND="ssh -p $SERVER_PORT root@$SERVER_HOST"
 
 if [ -n "$AGE_KEY" ]; then
   age_key=$($SSH_COMMAND 'nix-shell -p ssh-to-age --run "cat /etc/ssh/ssh_host_ed25519_key.pub | ssh-to-age"')
-  sed -i "s/&${AGE_KEY}.*$/\&${AGE_KEY} ${age_key}/" .sops.yaml
+  sed -i "s/&${AGE_KEY} .*$/\&${AGE_KEY} ${age_key}/" .sops.yaml
   for f in ./secrets/*; do
     sops updatekeys -y "$f" || true
   done
