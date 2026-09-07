957324d4e35da9e3126b47ed46b7ad86afe891d2
Jan van Brügge <supermanitu@gmail.com>
2026-09-07 23:36:23 +0200

    vps: Add rustical

--- vps/configuration.nix
+++ vps/configuration.nix
@@ -23,6 +23,28 @@
   services.fail2ban.enable = true;
   services.openssh.enable = true;
 
+  sops.secrets.rustical = {};
+  services.rustical = {
+    enable = true;
+    environmentFiles = [ "/run/secrets/rustical" ];
+    settings = {
+      http.bind = "127.0.0.1:4444";
+      frontend.allow_password_login = false;
+      oidc = {
+        name = "Authentik";
+        issuer = "https://authentik.${domain}/application/o/rustical/";
+        client_id = "FX0NSzJJ1QTjg8fvQewFcOtNvcdOhO3f8ABT1dm4";
+        claim_userid = "preferred_username";
+        scopes = [ "openid" "profile" "groups" ];
+        allow_sign_up = true;
+      };
+    };
+  };
+  ingress.rustical = {
+    subdomain = "calendar";
+    port = 4444;
+  };
+
   services.resolved.enable = true;
   networking.nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
   networking.hostName = "lighthouse";
